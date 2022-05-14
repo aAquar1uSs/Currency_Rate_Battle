ed151d40111d3b399a94edf2053365a01e1d6a37
Oleksandr Panchenko <panch@DESKTOP-NH15BJE>
2022-05-14 10:55:21 +0300

    Configured app config

--- src/CurrencyRateBattle_Server/Program.cs
+++ src/CurrencyRateBattle_Server/Program.cs
@@ -1,6 +1,42 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var host = builder.Host;
+
+host.ConfigureAppConfiguration(app =>
+    {
+        _ = app.AddJsonFile("appsettings.json", true, true)
+            .AddEnvironmentVariables();
+    })
+    .ConfigureLogging(loggerBuilder =>
+    {
+        _ = loggerBuilder.ClearProviders();
+        _ = loggerBuilder.AddSerilog(new LoggerConfiguration()
+            .WriteTo.File("AppLog.log")
+            .CreateLogger());
+    })
+    .ConfigureServices(services =>
+    {
+
+    });
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+    _ = app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.Run();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    _ = endpoints.MapControllers();
+});
+
+await app.RunAsync();
