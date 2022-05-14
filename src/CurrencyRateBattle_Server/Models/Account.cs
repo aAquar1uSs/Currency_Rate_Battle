560a2f51f727b02bf8d9a07239077e1439c58d58
Oleksandr Panchenko <panch@DESKTOP-NH15BJE>
2022-05-14 15:13:57 +0300

    Account model added

---
+++ src/CurrencyRateBattle_Server/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CurrencyRateBattle_Server.Models;
+
+public sealed class Account
+{
+    [Key]
+    public int Id { get;}
+
+    [Required]
+    public decimal Amount { get; set; }
+
+    public int AccountRef { get; set; }
+
+    public User User { get; set; }
+
+}
