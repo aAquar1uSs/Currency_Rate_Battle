b4ba4f6d0da0caf759c28fd0b52845313968589b
Oleksandr Panchenko <panch@DESKTOP-NH15BJE>
2022-05-16 14:09:56 +0300

    Login and registration page added

---
+++ src/CurrencyRateBattle_Client/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using CRBClient.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRBClient.Controllers;
+
+public class AccountController : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Login(User user)
+    {
+
+        return View();
+    }
+}
---
+++ src/CurrencyRateBattle_Client/Views/Account/Login.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRBClient.Views.Account;
+
+public class Login : PageModel
+{
+    [BindProperty]
+    [DisplayName("Email")]
+    public string Email { get; set; }
+
+    [BindProperty]
+    [DisplayName("Password")]
+    public string Password { get; set; }
+
+    [DisplayName("Confirm password")]
+    public string ConfirmPassword { get; set; }
+
+    public void OnGet()
+    {
+
+    }
+
+    public IActionResult OnPost()
+    {
+        return null;
+    }
+}
