e47292370a0c8c86c1c8d96c95d0820db2862387
Oleksandr Panchenko <panch@DESKTOP-NH15BJE>
2022-05-17 23:01:44 +0300

    Serilog added on the client side

--- src/CurrencyRateBattle_Client/Program.cs
+++ src/CurrencyRateBattle_Client/Program.cs
@@ -1,12 +1,22 @@
 ﻿using CRBClient.Helpers;
 using CRBClient.Services;
 using CRBClient.Services.Interfaces;
+using Serilog;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
+
+//Configure logger
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Logging.AddSerilog(logger)
+    .AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning);
+
 builder.Services.AddControllersWithViews();
 builder.Services.Configure<WebServerOptions>(
     builder.Configuration.GetSection(WebServerOptions.SectionName));
