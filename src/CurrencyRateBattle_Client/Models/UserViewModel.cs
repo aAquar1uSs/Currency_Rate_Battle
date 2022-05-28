e9e393500abb13b0d6fa43bd31bd09540ee97d54
Tanya Kravets <tkravets@dcse.com>
2022-05-29 02:43:46 +0300

    error handling on authorization page

--- src/CurrencyRateBattle_Client/Models/UserViewModel.cs
+++ src/CurrencyRateBattle_Client/Models/UserViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRBClient.Models;
@@ -8,20 +7,21 @@ namespace CRBClient.Models;
 public class UserViewModel
 {
     [BindProperty]
-    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress]
+    [Required(ErrorMessage = "Email is required.")]
     [DisplayName("Email")]
     public string Email { get; set; } = default!;
 
     [BindProperty]
     [DisplayName("Password")]
     [StringLength(30, MinimumLength = 6)]
-    [Required(ErrorMessage = "Password is required")]
+    [Required(ErrorMessage = "Password is required.")]
     public string Password { get; set; } = default!;
 
     [BindProperty]
     [DisplayName("Confirm password")]
     [StringLength(30, MinimumLength = 6)]
-    [Required(ErrorMessage = "Confirm Password required")]
+    [Required(ErrorMessage = "Confirm password is required.")]
     [Compare("Password", ErrorMessage = "Password doesn't match.")]
     public string? ConfirmPassword { get; set; }
 }
